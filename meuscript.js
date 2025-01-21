5308ab7347e41fef503814dbec0fe5ec8db79cc1
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-01-21 18:57:37 -0300

    ajustes

--- meuscript.js
+++ meuscript.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.getElementById("menu-toggle")
-    const navLinks = document.getElementById("ul_itens")
+const mobile_btn = document.getElementById('mobile-btn')
+const mobile_menu = document.getElementById('mobile-menu')
+let mobileIcon = mobile_btn.querySelector('i')
 
-    menuToggle.addEventListener("click", () => {
-        navLinks.classList.toggle("show")
-    });
-});
+mobile_btn.addEventListener('click',()=>{
+    mobile_menu.classList.toggle("active")
+    mobileIcon.classList.toggle("fa-x")
+})  
