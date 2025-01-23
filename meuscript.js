c2ee4e2f18b3584e8b8ab03dd27efe579848c0a0
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-01-23 15:48:35 -0300

    testando layouts

--- meuscript.js
+++ meuscript.js
@@ -6,3 +6,22 @@ mobile_btn.addEventListener('click',()=>{
     mobile_menu.classList.toggle("active")
     mobileIcon.classList.toggle("fa-x")
 })  
+
+const cards = document.querySelectorAll('.cards')
+const card_modal = document.getElementById('card_modal')
+
+cards.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        if(window.innerWidth >1200){
+        card_modal.style.display = 'block'
+    }else {
+        window.location.href = 'estoques_motos.html'
+    }
+    })
+})
+
+card_modal.addEventListener('click',(e)=>{
+    if(e.target === card_modal){
+        card_modal.style.display = 'none'
+    }
+})
